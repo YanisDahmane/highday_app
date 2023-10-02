c06ceeb8cf3f7b98c93ef0688c9b9ba1d57da00b
yanis dahmane <yanis.dahmane.pro@gmail.com>
2023-10-02 16:02:33 +0200

    Change Login system to use Axios and service

--- src/_helpers/auth-guard.js
+++ src/_helpers/auth-guard.js
@@ -1,10 +1,8 @@
 import router from '@/router';
 export function authGuard(to){
   let token = localStorage.getItem('token');
-  console.log(token);
   if(token){
     return true;
   }
-
   router.push('/login')
 }
